6841491c8caf90201bfe63a5d2317f5eef67fbdf
James Byrne <jamesdesbyrne@gmail.com>
2018-03-25 19:11:07 +0100

    Showing a restaurant on the map for RestaurantView

--- src/ui/components/RestaurantView/component.ts
+++ src/ui/components/RestaurantView/component.ts
@@ -1,4 +1,27 @@
 import Component from '@glimmer/component';
+import L from 'leaflet';
+
+import { getMapInstance, renderMap, addRestaurantToMap } from '../../../utils/map/map';
+import { setUserLocation } from '../../../utils/location/location';
 
 export default class RestaurantView extends Component {
+  map = {};
+
+  get element(): HTMLElement {
+    return this.bounds.firstNode as HTMLElement;
+  }
+
+  // Create and render the map
+  didInsertElement () {
+    const restaurant = this.args.restaurant;
+    const { latitude, longitude } = restaurant.location;
+    const ele = this.element.querySelector('#map');
+
+    const map = getMapInstance(ele, latitude, longitude, { zoomControl: false });
+
+    renderMap(map);
+    addRestaurantToMap(map, restaurant, () => {});
+
+    this.map = map;
+  }
 }
