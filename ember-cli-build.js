ad286426a895b7f6f3ea8c1addbcccf8b23dc2db
James Byrne <jamesdesbyrne@gmail.com>
2018-04-15 12:02:47 +0100

    Adding PWA configuration

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,7 @@
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
+const prod = process.env.EMBER_ENV === 'production';
 
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
@@ -11,7 +12,52 @@ module.exports = function(defaults) {
         resolve({ jsnext: true, module: true, main: true }),
         commonjs()
       ]
-    }
+    },
+    addons: {
+      blacklist: prod ? [] : ['ember-service-worker'],
+    },
+    'asset-cache': {
+      include: [
+        'assets/**/*',
+        '**/*',
+        'sw-registration.js',
+      ],
+      exclude: [
+        '**/*.txt',
+        '**/*.css',
+        'test.json',
+        'sw.js',
+      ],
+      version: 1,
+      requestMode: 'cors',
+    },
+    'esw-cache-fallback': {
+      patterns: [
+        'https://developers.zomato.com/api/(.+)',
+      ],
+    },
+    minifyJS: {
+      enabled: prod,
+      options: {
+        mangle: true,
+      },
+    },
+    minifyCSS: {
+      enabled: prod,
+    },
+    inlineContent: {
+      'critical-css': 'src/ui/styles/app.css',
+    },
+    minifyHTML: {
+      enabled: prod,
+      htmlFiles: ['index.html'],
+      minifierOptions: {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+      },
+    },
   });
 
   return app.toTree();
