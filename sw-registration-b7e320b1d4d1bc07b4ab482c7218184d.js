25633201653441af2933b90628e46b8c46a6772a
James Byrne <jamesdesbyrne@gmail.com>
2018-04-20 17:08:19 +0100

    Deployed 5f8a892

--- sw-registration-b7e320b1d4d1bc07b4ab482c7218184d.js
+++ sw-registration-b7e320b1d4d1bc07b4ab482c7218184d.js
@@ -1,5 +1,5 @@
 (function(){"use strict"
 var e=[],r=[]
-"serviceWorker"in navigator&&navigator.serviceWorker.register("/glimmer_eatssw.js",{scope:"/glimmer_eats"}).then(function(r){for(var n=Promise.resolve(),t=0;t<e.length;t++)(function(t){n=n.then(function(){return e[t](r)})})(t)
+"serviceWorker"in navigator&&navigator.serviceWorker.register("/glimmer_eats/sw.js",{scope:"/glimmer_eats/"}).then(function(r){for(var n=Promise.resolve(),t=0;t<e.length;t++)(function(t){n=n.then(function(){return e[t](r)})})(t)
 return n.then(function(){console.log("Service Worker registration succeeded. Scope is "+r.scope)})}).catch(function(e){for(var n=Promise.resolve(),t=0;t<r.length;t++)(function(t){n=n.then(function(){return r[t](e)})})(t)
 return n.then(function(){console.log("Service Worker registration failed with "+e)})})})()
