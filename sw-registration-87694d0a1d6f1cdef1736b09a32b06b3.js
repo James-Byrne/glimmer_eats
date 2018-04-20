3c341074c3015f5159178f11f2e49661476b360e
James Byrne <jamesdesbyrne@gmail.com>
2018-04-20 14:42:41 +0100

    Deployed 5f8a892

--- sw-registration-87694d0a1d6f1cdef1736b09a32b06b3.js
+++
@@ -1,5 +0,0 @@
-(function(){"use strict"
-var e=[],r=[]
-"serviceWorker"in navigator&&navigator.serviceWorker.register("/glimmer_eatssw.js",{scope:"/glimmer_eats"}).then(function(r){for(var n=Promise.resolve(),t=0;t<e.length;t++)(function(t){n=n.then(function(){return e[t](r)})})(t)
-return n.then(function(){console.log("Service Worker registration succeeded. Scope is "+r.scope)})}).catch(function(e){for(var n=Promise.resolve(),t=0;t<r.length;t++)(function(t){n=n.then(function(){return r[t](e)})})(t)
-return n.then(function(){console.log("Service Worker registration failed with "+e)})})})()
